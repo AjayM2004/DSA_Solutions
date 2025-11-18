134999e73c4cd01c53e2c3af725438a8693fdee8
AjayM2004 <ajay97908940@gmail.com>
2025-11-18 15:47:20 +0530

    adding trees

--- src/tree/bt/basics/Q1MaxDepth.java
+++ src/tree/bt/basics/Q1MaxDepth.java
@@ -14,8 +14,8 @@ public class Q1MaxDepth {
         return 1 + Math.max(lh,rh);
     }
     public static void main(String[] args) {
-        List<Integer> arr1 = new ArrayList<>(Arrays.asList(new Integer[]{3,9,20,null,null,15,7}));
-        List<Integer> arr2 = new ArrayList<>(Arrays.asList(new Integer[]{1,null,2}));
+        List<Integer> arr1 = new ArrayList<>(Arrays.asList(3,9,20,null,null,15,7));
+        List<Integer> arr2 = new ArrayList<>(Arrays.asList(1,null,2));
         TreeNode root1 = Tree.buildTree(arr1);
         TreeNode root2 = Tree.buildTree(arr2);
         System.out.println("Max depth of tree 1 = "+maxDepth(root1));
--- src/tree/bt/basics/Q2IsBalanced.java
+++ src/tree/bt/basics/Q2IsBalanced.java
@@ -1,4 +1,38 @@
 package tree.bt.basics;
-
+import java.util.*;
 public class Q2IsBalanced {
+    static int maxDepth(TreeNode root)
+    {
+        if(root==null)
+        {
+            return 0;
+        }
+        int lh = maxDepth(root.left);
+        if(lh==-1)
+        {
+            return -1;
+        }
+        int rh = maxDepth(root.right);
+        if(rh==-1)
+        {
+            return -1;
+        }
+        if(Math.abs(lh-rh)>1)
+        {
+            return -1;
+        }
+        return 1+Math.max(lh,rh);
+    }
+    static boolean isBalanced(TreeNode root)
+    {
+        return root == null || maxDepth(root) != -1;
+    }
+    public static void main(String[] args) {
+        List<Integer> arr1 = new ArrayList<>(Arrays.asList(3,9,20,null,null,15,7));
+        List<Integer> arr2 = new ArrayList<>(Arrays.asList(1,2,2,3,3,null,null,4,4));
+        TreeNode root1 = Tree.buildTree(arr1);
+        TreeNode root2 = Tree.buildTree(arr2);
+        System.out.println("isBalanced tree -> "+isBalanced(root1));
+        System.out.println("isBalanced tree -> "+isBalanced(root2));
+    }
 }
